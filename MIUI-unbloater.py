bc469d7316285f6843d81a04b5a11e8c4300eee2
barteqcz <118764028+barteqcz@users.noreply.github.com>
2023-05-06 14:04:09 +0200

    Update MIUI-unbloater.py

--- MIUI-unbloater.py
+++ MIUI-unbloater.py
@@ -2,7 +2,28 @@ import os
 import subprocess
 
 try:
-    print("Make sure you connected the phone to the PC via USB, enabled USB debugging, and clicked ok in the windows that allows USB debugging from this PC.\n")
+    
+    def check_adb_exists():
+        result = subprocess.run(['adb', 'version'], capture_output=True, text=True)
+        output = result.stderr.strip()
+        if 'command not found' in output or 'is not recognized' in output:
+            return False
+        return True
+
+    def check_adb_devices():
+        result = subprocess.run(['adb', 'devices'], capture_output=True, text=True)
+        output = result.stdout.strip()
+        if 'List of devices attached' in output:
+            return True
+        return False
+
+    if not check_adb_exists():
+        print("ADB is not installed or not accessible. Please make sure ADB is installed and added to the system's PATH.")
+        quit()
+
+    if not check_adb_devices():
+        print("No devices found. Please make sure your phone is connected and USB debugging is enabled.")
+        quit()
 
     while True:
         response = input("Do you wanna uninstall Mi Music? [Y/n] ")
